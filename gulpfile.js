ba643179da160fd881dab1114f99c31b08ffc4d5
Gary Lo <logap@me.com>
2016-08-14 13:36:50 +0800

    abolish gulp dev env

--- gulpfile.js
+++ gulpfile.js
@@ -17,58 +17,91 @@ const gutil = require('gulp-util');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const rename = require('gulp-rename')
+const ngAnnotate = require('gulp-ng-annotate');
+const minifyCss = require('gulp-minify-css')
+const runSequence = require('run-sequence');
+
+function onError(err) {
+  console.log(err.message)
+  this.emit('end')
+}
 
 gulp.task('browserify', function () {
   // set up the browserify instance on a task basis
   var b = browserify({
     entries: './src/es6/app.js',
-    debug: true,
+    insertGlobals : true,
+    debug: false,
     // defining transforms here will avoid crashing your stream
     transform: [
-        babelify.configure({
-          presets: ["es2015"]
-        })
+      babelify.configure({
+        presets: ["es2015"]
+      })
     ]
-  });
+  })
 
-  return b.bundle()
+  return b
+      .bundle()
+      .on('error', onError)
       .pipe(source('app.js'))
       .pipe(buffer())
       .pipe(sourcemaps.init({loadMaps: true}))
       // Add transformation tasks to the pipeline here.
-      .on('error', gutil.log)
       .pipe(sourcemaps.write('./'))
-      .pipe(gulp.dest('./www/js/'));
-});
-
-gulp.task('compress', () => {
-  gulp.src('./www/js/app.js')
-      .pipe(stripDebug()) // remove debug log
-      .pipe(uglify({mangle: false}))
-      .pipe(rename({suffix: '.min'}))
-      .on('error', gutil.log)
       .pipe(gulp.dest('./www/js/'))
 })
 
-gulp.task('release',['browserify','sass'])
 
-gulp.task('sass', function(done) {
-  gulp.src(['./src/scss/app.scss'])
-      .pipe(sass())
-      .on('error', sass.logError)
-      .pipe(gulp.dest('./www/css/'))
-      .on('end', done);
-})
-
-gulp.task('run', ['release'], () => {
-  gulp.watch(['src/es6/**/*.js'], ['browserify'])
-  gulp.watch(['./src/scss/**/*'], ['sass'])
-
+gulp.task('webserver',() => {
   return gulp.src('./www')
       .pipe(webserver({
         host: '0.0.0.0',
         port: 3000,
         livereload: true,
         open: true,
-      }));
+      }))
+})
+
+gulp.task('sass', function() {
+  return gulp.src(['./src/scss/app.scss'])
+      .pipe(sass())
+      .on('error', sass.logError)
+      .pipe(gulp.dest('./www/css/'))
+})
+
+gulp.task('watchJs', function() {
+  return gulp.watch(['./src/es6/**/*'], ['browserify'])
+})
+
+gulp.task('watchSass', function() {
+  return gulp.watch(['./src/scss/**/*'], ['sass'])
+})
+
+gulp.task('watch', ['watchJs','watchSass'])
+
+gulp.task('compressCss', function() {
+  return gulp.src(['./www/css/app.css'])
+      .pipe(minifyCss({
+        keepSpecialComments: 0
+      }))
+      .pipe(gulp.dest('./www/css/'))
+})
+gulp.task('compressJs', function() {
+  return gulp.src('./www/js/app.js')
+      .pipe(ngAnnotate())
+      .pipe(stripDebug())
+      .pipe(uglify({mangle: false}))
+      .pipe(gulp.dest('./www/js/'))
+})
+
+gulp.task('build', (cb) => {
+  return runSequence(['browserify','sass'],cb)
+})
+
+gulp.task('release', (cb) => {
+  return runSequence('build',['compressJs','compressCss'],cb)
+})
+
+gulp.task('run', (cb) => {
+  return runSequence('build','watch','webserver',cb)
 })
